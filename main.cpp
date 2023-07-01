32b7b36f038b4c3d6e7c858cd18bca738a3a3288
EMCJava <loyusum246807952@gmail.com>
2023-07-02 00:57:45 +0800

    Added simple glfw program

--- main.cpp
+++ main.cpp
@@ -3,9 +3,44 @@
 #include <exception>
 #include <iostream>
 
+// #define GLFW_INCLUDE_NONE
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+
 int
 main()
 {
+
+	if (!glfwInit())
+	{
+		throw std::runtime_error("Failed to initialize GLFW");
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	GLFWwindow *window = glfwCreateWindow(640, 480, "LMCEngine", nullptr, nullptr);
+	if (!window)
+	{
+		throw std::runtime_error("Failed to create GLFW window");
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+
+	while (!glfwWindowShouldClose(window))
+	{
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	return 0;
+
+	/////////////////////////
+
 	Engine engine;
 
 	try {
