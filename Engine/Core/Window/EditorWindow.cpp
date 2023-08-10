4d261e3569f436f1af3283a4c74235d657e11e9f
EMCJava <loyusum246807952@gmail.com>
2023-08-10 22:18:38 +0800

    Added dummy Details window

--- Engine/Core/Window/EditorWindow.cpp
+++ Engine/Core/Window/EditorWindow.cpp
@@ -4,21 +4,152 @@
 
 #include "EditorWindow.hpp"
 
-#include <imgui.h>
-
-// #define GLFW_INCLUDE_NONE
-#include <glad/gl.h>
-#include <GLFW/glfw3.h>
-
-#include <spdlog/spdlog.h>
-
 #include <Engine/Core/Graphic/HotReloadFrameBuffer/HotReloadFrameBuffer.hpp>
 #include <Engine/Core/Exception/Runtime/ImGuiContextInvalid.hpp>
 #include <Engine/Core/File/OSFile.hpp>
 #include <Engine/Core/Project/Project.hpp>
 #include <Engine/Engine.hpp>
 
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+
+#include <spdlog/spdlog.h>
+
 #include <filesystem>
+
+#define IMGUI_DEFINE_MATH_OPERATORS
+#include "imgui_internal.h"
+#include <imgui.h>
+
+// https://github.com/ocornut/imgui/issues/1496
+namespace
+{
+static ImVector<ImRect> s_GroupPanelLabelStack;
+
+void
+BeginGroupPanel( const char* name, const ImVec2& size )
+{
+    ImGui::BeginGroup( );
+
+    auto cursorPos   = ImGui::GetCursorScreenPos( );
+    auto itemSpacing = ImGui::GetStyle( ).ItemSpacing;
+    ImGui::PushStyleVar( ImGuiStyleVar_FramePadding, ImVec2( 0.0f, 0.0f ) );
+    ImGui::PushStyleVar( ImGuiStyleVar_ItemSpacing, ImVec2( 0.0f, 0.0f ) );
+
+    auto frameHeight = ImGui::GetFrameHeight( );
+    ImGui::BeginGroup( );
+
+    ImVec2 effectiveSize = size;
+    if ( size.x < 0.0f )
+        effectiveSize.x = ImGui::GetContentRegionAvail( ).x;
+    else
+        effectiveSize.x = size.x;
+    ImGui::Dummy( ImVec2( effectiveSize.x, 0.0f ) );
+
+    ImGui::Dummy( ImVec2( frameHeight * 0.5f, 0.0f ) );
+    ImGui::SameLine( 0.0f, 0.0f );
+    ImGui::BeginGroup( );
+    ImGui::Dummy( ImVec2( frameHeight * 0.5f, 0.0f ) );
+    ImGui::SameLine( 0.0f, 0.0f );
+    ImGui::TextUnformatted( name );
+    auto labelMin = ImGui::GetItemRectMin( );
+    auto labelMax = ImGui::GetItemRectMax( );
+    ImGui::SameLine( 0.0f, 0.0f );
+    ImGui::Dummy( ImVec2( 0.0, frameHeight + itemSpacing.y ) );
+    ImGui::BeginGroup( );
+
+    // ImGui::GetWindowDrawList()->AddRect(labelMin, labelMax, IM_COL32(255, 0, 255, 255));
+
+    ImGui::PopStyleVar( 2 );
+
+#if IMGUI_VERSION_NUM >= 17301
+    ImGui::GetCurrentWindow( )->ContentRegionRect.Max.x -= frameHeight * 0.5f;
+    ImGui::GetCurrentWindow( )->WorkRect.Max.x -= frameHeight * 0.5f;
+    ImGui::GetCurrentWindow( )->InnerRect.Max.x -= frameHeight * 0.5f;
+#else
+    ImGui::GetCurrentWindow( )->ContentsRegionRect.Max.x -= frameHeight * 0.5f;
+#endif
+    ImGui::GetCurrentWindow( )->Size.x -= frameHeight;
+
+    auto itemWidth = ImGui::CalcItemWidth( );
+    ImGui::PushItemWidth( ImMax( 0.0f, itemWidth - frameHeight ) );
+
+    s_GroupPanelLabelStack.push_back( ImRect( labelMin, labelMax ) );
+}
+
+void
+EndGroupPanel( )
+{
+    ImGui::PopItemWidth( );
+
+    auto itemSpacing = ImGui::GetStyle( ).ItemSpacing;
+
+    ImGui::PushStyleVar( ImGuiStyleVar_FramePadding, ImVec2( 0.0f, 0.0f ) );
+    ImGui::PushStyleVar( ImGuiStyleVar_ItemSpacing, ImVec2( 0.0f, 0.0f ) );
+
+    auto frameHeight = ImGui::GetFrameHeight( );
+
+    ImGui::EndGroup( );
+
+    // ImGui::GetWindowDrawList()->AddRectFilled(ImGui::GetItemRectMin(), ImGui::GetItemRectMax(), IM_COL32(0, 255, 0, 64), 4.0f);
+
+    ImGui::EndGroup( );
+
+    ImGui::SameLine( 0.0f, 0.0f );
+    ImGui::Dummy( ImVec2( frameHeight * 0.5f, 0.0f ) );
+    ImGui::Dummy( ImVec2( 0.0, frameHeight - frameHeight * 0.5f - itemSpacing.y ) );
+
+    ImGui::EndGroup( );
+
+    auto itemMin = ImGui::GetItemRectMin( );
+    auto itemMax = ImGui::GetItemRectMax( );
+    // ImGui::GetWindowDrawList()->AddRectFilled(itemMin, itemMax, IM_COL32(255, 0, 0, 64), 4.0f);
+
+    auto labelRect = s_GroupPanelLabelStack.back( );
+    s_GroupPanelLabelStack.pop_back( );
+
+    ImVec2 halfFrame = ImVec2( frameHeight * 0.25f, frameHeight ) * 0.5f;
+    ImRect frameRect = ImRect( itemMin + halfFrame, itemMax - ImVec2( halfFrame.x, 0.0f ) );
+    labelRect.Min.x -= itemSpacing.x;
+    labelRect.Max.x += itemSpacing.x;
+    for ( int i = 0; i < 4; ++i )
+    {
+        switch ( i )
+        {
+        // left half-plane
+        case 0: ImGui::PushClipRect( ImVec2( -FLT_MAX, -FLT_MAX ), ImVec2( labelRect.Min.x, FLT_MAX ), true ); break;
+        // right half-plane
+        case 1: ImGui::PushClipRect( ImVec2( labelRect.Max.x, -FLT_MAX ), ImVec2( FLT_MAX, FLT_MAX ), true ); break;
+        // top
+        case 2: ImGui::PushClipRect( ImVec2( labelRect.Min.x, -FLT_MAX ), ImVec2( labelRect.Max.x, labelRect.Min.y ), true ); break;
+        // bottom
+        case 3: ImGui::PushClipRect( ImVec2( labelRect.Min.x, labelRect.Max.y ), ImVec2( labelRect.Max.x, FLT_MAX ), true ); break;
+        }
+
+        ImGui::GetWindowDrawList( )->AddRect(
+            frameRect.Min, frameRect.Max,
+            ImColor( ImGui::GetStyleColorVec4( ImGuiCol_Border ) ),
+            halfFrame.x );
+
+        ImGui::PopClipRect( );
+    }
+
+    ImGui::PopStyleVar( 2 );
+
+#if IMGUI_VERSION_NUM >= 17301
+    ImGui::GetCurrentWindow( )->ContentRegionRect.Max.x += frameHeight * 0.5f;
+    ImGui::GetCurrentWindow( )->WorkRect.Max.x += frameHeight * 0.5f;
+    ImGui::GetCurrentWindow( )->InnerRect.Max.x += frameHeight * 0.5f;
+#else
+    ImGui::GetCurrentWindow( )->ContentsRegionRect.Max.x += frameHeight * 0.5f;
+#endif
+    ImGui::GetCurrentWindow( )->Size.x += frameHeight;
+
+    ImGui::Dummy( ImVec2( 0.0f, 0.0f ) );
+
+    ImGui::EndGroup( );
+}
+}   // namespace
 
 void
 EditorWindow::Update( )
@@ -209,7 +340,16 @@ EditorWindow::UpdateImGui( )
 
         if ( m_ConceptInspectionCache.m_SelectedConcept != nullptr )
         {
-            ImGui::Text( "Concept type - %s -", m_ConceptInspectionCache.m_SelectedConcept->GetName( ) );
+            const auto* Name = m_ConceptInspectionCache.m_SelectedConcept->GetName( );
+            
+            ::BeginGroupPanel( Name, ImVec2 { -1, 0 } );
+
+            static float data[ 3 ] { 0 };
+            ImGui::InputFloat3( "Coordinate", data );
+
+            ImGui::Spacing( );
+
+            ::EndGroupPanel( );
         }
 
         ImGui::End( );
