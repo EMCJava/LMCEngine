d031fd837dd55908f356ed7454c7f3d1daa0939f
EMCJava <loyusum246807952@gmail.com>
2023-09-07 18:18:31 +0800

    Removed unused shader

--- test/ADanceOfFireAndIce/Source/GameManager/Private/GameManager.cpp
+++ test/ADanceOfFireAndIce/Source/GameManager/Private/GameManager.cpp
@@ -1306,27 +1306,6 @@ std::vector<TileSpriteSet::TileMeta> TmpMap {
 
 }
 
-const char* vertexTextureShaderSource   = "#version 330 core\n"
-                                          "layout (location = 0) in vec3 aPos;\n"
-                                          "layout (location = 1) in vec2 aTexCoord;\n"
-                                          "out vec2 TexCoord;\n"
-                                          "uniform mat4 projectionMatrix;\n"
-                                          "uniform mat4 modelMatrix;\n"
-                                          "void main()\n"
-                                          "{\n"
-                                          "   gl_Position = projectionMatrix * modelMatrix * vec4(aPos, 1.0);\n"
-                                          "   TexCoord = aTexCoord;\n"
-                                          "}\0";
-const char* fragmentTextureShaderSource = "#version 330 core\n"
-                                          "out vec4 FragColor;\n"
-                                          "in vec2 TexCoord;\n"
-                                          "uniform sampler2D sample_texture;\n"
-                                          "void main()\n"
-                                          "{\n"
-                                          "   vec4 texColor = texture(sample_texture, TexCoord);\n"
-                                          "   FragColor = texColor;\n"
-                                          "}\n\0";
-
 GameManager::GameManager( )
 {
     spdlog::info( "GameManager concept constructor called" );
