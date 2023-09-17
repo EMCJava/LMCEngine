7b1b525d04c890610e41914f1ace2c41b61271a8
EMCJava <loyusum246807952@gmail.com>
2023-09-17 16:58:39 +0800

    Move GAME_CONFIG_PATH macro to defines.cmake

--- cmake/defines.cmake
+++ cmake/defines.cmake
@@ -12,4 +12,5 @@ else ()
     add_compile_definitions("RootConceptIncludePath=\"${ROOT_CONCEPT_PATH}\"")
 endif ()
 
+add_compile_definitions("GAME_CONFIG_PATH=\"${GAME_CONFIG_PATH}\"")
 add_compile_definitions("CMAKE_BUILD_TYPE=\"${CMAKE_BUILD_TYPE}\"")
