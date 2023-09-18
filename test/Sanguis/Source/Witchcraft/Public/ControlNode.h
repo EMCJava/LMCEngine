67167cd6aa2eaf9c4e90f18c852ca0db109c8148
EMCJava <loyusum246807952@gmail.com>
2023-09-19 06:25:06 +0800

    SaControlNode default constructor

--- test/Sanguis/Source/Witchcraft/Public/ControlNode.h
+++ test/Sanguis/Source/Witchcraft/Public/ControlNode.h
@@ -11,7 +11,7 @@ class SaControlNode
 {
 public:
     virtual ~SaControlNode( ) = default;
-    SaControlNode( ) = default;
+    SaControlNode( )          = default;
 
     SaEffect GetEffect( )
     {
