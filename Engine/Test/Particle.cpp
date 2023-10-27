2b70334d52d36837233fd8e34c42749767519735
EMCJava <loyusum246807952@gmail.com>
2023-10-27 09:39:55 +0800

    Added particle test

---
+++ Engine/Test/Particle.cpp
@@ -0,0 +1,11 @@
+//
+// Created by samsa on 10/27/2023.
+//
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/benchmark/catch_benchmark.hpp>
+
+TEST_CASE( "Particle", "[Particle]" )
+{
+    REQUIRE( true );
+}
