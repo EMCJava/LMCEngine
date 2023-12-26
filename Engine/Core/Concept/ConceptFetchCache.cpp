40dc5136b890b18ab0ce4689bda103fdd3876bdc
EMCJava <loyusum246807952@gmail.com>
2023-12-26 14:38:59 +0800

    Use inl file on ConceptSetFetchCache

--- Engine/Core/Concept/ConceptFetchCache.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by loyus on 7/16/2023.
-//
-
-#include "ConceptSetFetchCache.hpp"
--- Engine/Core/Concept/ConceptSetFetchCache.hpp
+++ Engine/Core/Concept/ConceptSetFetchCache.hpp
@@ -36,45 +36,4 @@ private:
     friend class ConceptList;
 };
 
-template <typename Ty>
-uint64_t
-ConceptSetFetchCache<Ty>::GetHash( ) const noexcept
-{
-    return m_CacheHash;
-}
-
-template <typename Ty>
-void
-ConceptSetFetchCache<Ty>::ForEach( auto&& F )
-{
-    for ( auto& C : m_CachedConcepts )
-    {
-        F( C );
-    }
-}
-
-template <typename Ty>
-void
-ConceptSetFetchCache<Ty>::ForEachIndex( auto&& F )
-{
-    for ( size_t i = 0; i < m_CachedConcepts.size( ); ++i )
-    {
-        F( i, m_CachedConcepts[ i ] );
-    }
-}
-
-
-template <typename Ty>
-bool
-ConceptSetFetchCache<Ty>::NotEmpty( )
-{
-    return !m_CachedConcepts.empty( );
-}
-
-template <typename Ty>
-void
-ConceptSetFetchCache<Ty>::Clear( )
-{
-    m_CacheHash = 0;
-    m_CachedConcepts.clear( );
-}
+#include "ConceptSetFetchCache.inl"
