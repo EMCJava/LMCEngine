4b503081cc6a353a09a727a3632abe3be2fc1e2e
EMCJava <loyusum246807952@gmail.com>
2024-03-19 18:14:20 +0800

    Added GroupParticipant in ServerSectionGroup

--- test/Sanguis/Source/GameServer/Private/ServerSectionGroup.cpp
+++ test/Sanguis/Source/GameServer/Private/ServerSectionGroup.cpp
@@ -3,3 +3,22 @@
 //
 
 #include "ServerSectionGroup.hpp"
+
+void
+SanguisNet::ServerSectionGroup::Join( const std::shared_ptr<GroupParticipant>& Participant )
+{
+    m_Participants.insert( Participant );
+}
+
+void
+SanguisNet::ServerSectionGroup::Leave( const std::shared_ptr<GroupParticipant>& participant )
+{
+    m_Participants.erase( participant );
+}
+
+void
+SanguisNet::ServerSectionGroup::Deliver( const SanguisNet::Message& Msg ) const
+{
+    for ( const auto& Participant : m_Participants )
+        Participant->Deliver( Msg );
+}
--- test/Sanguis/Source/GameServer/Public/ServerSectionGroup.hpp
+++ test/Sanguis/Source/GameServer/Public/ServerSectionGroup.hpp
@@ -5,7 +5,22 @@
 
 #pragma once
 
+#include "GroupParticipant.hpp"
 
+#include <set>
+
+namespace SanguisNet
+{
 class ServerSectionGroup
 {
+public:
+    void Join( const std::shared_ptr<GroupParticipant>& Participant );
+
+    void Leave( const std::shared_ptr<GroupParticipant>& participant );
+
+    void Deliver( const Message& Msg ) const;
+
+private:
+    std::set<std::shared_ptr<GroupParticipant>> m_Participants;
 };
+}   // namespace SanguisNet
