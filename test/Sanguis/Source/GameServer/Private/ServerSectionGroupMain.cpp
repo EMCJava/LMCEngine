34a885e85232578dd98cbf990e80921239f7ff6b
EMCJava <loyusum246807952@gmail.com>
2024-03-22 03:37:28 +0800

    Added ServerSectionGroupMain

---
+++ test/Sanguis/Source/GameServer/Private/ServerSectionGroupMain.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Lo Yu Sum on 2024/03/22.
+//
+
+#include "ServerSectionGroupMain.hpp"
---
+++ test/Sanguis/Source/GameServer/Public/ServerSectionGroupMain.hpp
@@ -0,0 +1,15 @@
+//
+// Created by Lo Yu Sum on 2024/03/22.
+//
+
+
+#pragma once
+
+#include "ServerSectionGroup.hpp"
+
+namespace SanguisNet
+{
+class ServerSectionGroupMain : public ServerSectionGroup
+{
+};
+}   // namespace SanguisNet
